56640ab0cb20ab9355d53c028397a133dd6a5fee
natlxh <A01667411@tec.mx>
2025-10-30 11:33:55 -0600

    - Se agrego una varaible ghost_veloctiy - Se agrego dicha variable a cada vector para defiir la velocidad

--- pacman.py
+++ pacman.py
@@ -2,17 +2,21 @@ from random import choice
 from turtle import *
 from freegames import floor, vector
 
+
 state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
+ghost_velocity = 8
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+   [vector(-180, 160), vector(ghost_velocity, 0)],
+   [vector(-180, -160), vector(0, ghost_velocity)],
+   [vector(100, 160), vector(0, -ghost_velocity)],
+   [vector(100, -160), vector(-ghost_velocity, 0)],
 ]
+
+
 tiles = [
    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
    0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,
@@ -37,82 +41,103 @@ tiles = [
 ]
 
 
+
+
 def square(x, y):
-    "Draw square using path at (x, y)."
-    path.up()
-    path.goto(x, y)
-    path.down()
-    path.begin_fill()
+   "Draw square using path at (x, y)."
+   path.up()
+   path.goto(x, y)
+   path.down()
+   path.begin_fill()
 
-    for count in range(4):
-        path.forward(20)
-        path.left(90)
 
-    path.end_fill()
+   for count in range(4):
+       path.forward(20)
+       path.left(90)
+
+
+   path.end_fill()
+
 
 def offset(point):
-    "Return offset of point in tiles."
-    x = (floor(point.x, 20) + 200) / 20
-    y = (180 - floor(point.y, 20)) / 20
-    index = int(x + y * 20)
-    return index
+   "Return offset of point in tiles."
+   x = (floor(point.x, 20) + 200) / 20
+   y = (180 - floor(point.y, 20)) / 20
+   index = int(x + y * 20)
+   return index
+
 
 def valid(point):
-    "Return True if point is valid in tiles."
-    index = offset(point)
+   "Return True if point is valid in tiles."
+   index = offset(point)
 
-    if tiles[index] == 0:
-        return False
 
-    index = offset(point + 19)
+   if tiles[index] == 0:
+       return False
 
-    if tiles[index] == 0:
-        return False
 
-    return point.x % 20 == 0 or point.y % 20 == 0
+   index = offset(point + 19)
+
+
+   if tiles[index] == 0:
+       return False
+
+
+   return point.x % 20 == 0 or point.y % 20 == 0
+
 
 def world():
-    "Draw world using path."
-    bgcolor('black')
-    path.color('blue')
+   "Draw world using path."
+   bgcolor('black')
+   path.color('blue')
 
-    for index in range(len(tiles)):
-        tile = tiles[index]
 
-        if tile > 0:
-            x = (index % 20) * 20 - 200
-            y = 180 - (index // 20) * 20
-            square(x, y)
+   for index in range(len(tiles)):
+       tile = tiles[index]
 
-            if tile == 1:
-                path.up()
-                path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
+
+       if tile > 0:
+           x = (index % 20) * 20 - 200
+           y = 180 - (index // 20) * 20
+           square(x, y)
+
+
+           if tile == 1:
+               path.up()
+               path.goto(x + 10, y + 10)
+               path.dot(2, 'white')
+
 
 def move():
-    "Move pacman and all ghosts."
-    writer.undo()
-    writer.write(state['score'])
+   "Move pacman and all ghosts."
+   writer.undo()
+   writer.write(state['score'])
 
-    clear()
 
-    if valid(pacman + aim):
-        pacman.move(aim)
+   clear()
 
-    index = offset(pacman)
 
-    if tiles[index] == 1:
-        tiles[index] = 2
-        state['score'] += 1
-        x = (index % 20) * 20 - 200
-        y = 180 - (index // 20) * 20
-        square(x, y)
+   if valid(pacman + aim):
+       pacman.move(aim)
 
-    up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
 
-    for point, course in ghosts:
+   index = offset(pacman)
+
+
+   if tiles[index] == 1:
+       tiles[index] = 2
+       state['score'] += 1
+       x = (index % 20) * 20 - 200
+       y = 180 - (index // 20) * 20
+       square(x, y)
+
+
+   up()
+   goto(pacman.x + 10, pacman.y + 10)
+   dot(20, 'yellow')
+
+
+   for point, course in ghosts:
        if valid(point + course):
            point.move(course)
        else:
@@ -133,23 +158,30 @@ def move():
                course.x, course.y = plan.x, plan.y
 
 
+
+
        up()
        goto(point.x + 10, point.y + 10)
        dot(20, 'red')
 
-    update()
 
-    for point, course in ghosts:
-        if abs(pacman - point) < 20:
-            return
+   update()
 
-    ontimer(move, 100)
+
+   for point, course in ghosts:
+       if abs(pacman - point) < 20:
+           return
+
+
+   ontimer(move, 100)
+
 
 def change(x, y):
-    "Change pacman aim if valid."
-    if valid(pacman + vector(x, y)):
-        aim.x = x
-        aim.y = y
+   "Change pacman aim if valid."
+   if valid(pacman + vector(x, y)):
+       aim.x = x
+       aim.y = y
+
 
 setup(420, 420, 370, 0)
 hideturtle()
